af32ac3142e221f1bd63e602a95c0605bc88537f
Mike Looijmans <mike.looijmans@topic.nl>
2017-07-05 15:52:36 +0200

    miamil2: Add dyplo support to BSP
    Define Dyplo address and IRQ in BSP. To use this, the application must
    include <bsp/dyplo.h> in system.h or a similar location.

---
+++ c/src/lib/libbsp/sparc/miamil2/include/dyplo.h
@@ -0,0 +1,13 @@
+/*
+ * DYPLO configuration
+ * COPYRIGHT (c) 2017.
+ * Topic Embedded Products
+ */
+
+#ifndef __BSP_DYPLO_H
+#define __BSP_DYPLO_H
+
+#define BSP_DYPLO_BASE_ADDR 0xD0000000	
+#define BSP_DYPLO_IRQ 12
+
+#endif
